9a70abead0623908bae7248839af077489108f3e
DeclanParsons <declanparsons0@gmail.com>
2023-10-24 12:47:44 -0400

    Updated baud read

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.io.IOException;
 import java.io.InputStream;
-
 import com.fazecast.jSerialComm.SerialPort;
+
 
 class Main{ 
 
@@ -16,17 +16,17 @@ class Main{
 
         SerialPort[] ports = SerialPort.getCommPorts(); 
         SerialPort single_port = ports[0]; 
-        
+
         /*
         take first element of comm ports list eventually add feature for user select
         */
-
-        single_port.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 0, 0); //stops timeout error
-        single_port.setBaudRate(9600); //add flexibility with baud rate, find baud based on port?
+        single_port.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 0, 0); //stops timeout error if == 0 
+        single_port.setBaudRate(single_port.getBaudRate()); //add flexibility with baud rate, find baud based on port? (Hope this works)
 
         read_port(single_port); 
                         
     }
+
 
     static void read_port(SerialPort port){ 
 
@@ -34,17 +34,16 @@ class Main{
 
         try{ 
             while(true){ 
+
                 int data = in.read(); 
                 System.out.print((char) data); 
             }
         }catch(IOException e){ 
+
             e.printStackTrace(); 
         }finally{ 
+
             port.closePort(); 
         }
-
     }
-
-
-
 }
